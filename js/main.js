43c87aab7f098deb8892613ba8f925740c5d0b8c
bridenko <glomokomo@gmail.com>
2021-12-30 21:49:59 +0200

    pre-final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* Header background - start */
+
 ( function () {
     const header = document.querySelector('.header');
     window.onscroll = () => {
@@ -20,3 +22,22 @@
         else { header.classList.remove ('js-item-border-opacity-50'); }
     }
 } () )
+
+/* Header background - end */
+
+/* Burger menu - start */
+
+( function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.main-menu');
+    const menuCloseItem = document.querySelector('.main-menu__close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('main-menu__active')
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('main-menu__active')
+    });
+} () )
+
+/* Burger menu - end */
+
