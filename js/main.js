67c6d3d823cf8a1b249606b951d6e8c08b44917f
bridenko <glomokomo@gmail.com>
2021-12-31 20:38:54 +0200

    final

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
         if (window.pageYOffset > 40) { header.classList.add ('js-item-border-opacity-50'); }
         else { header.classList.remove ('js-item-border-opacity-50'); }
     }
-} () )
+} () );
 
 /* Header background - end */
 
@@ -37,7 +37,51 @@
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('main-menu__active')
     });
-} () )
+} () );
 
 /* Burger menu - end */
 
+
+/* Scroll for item menu - start */
+
+// Scroll to anchors
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+    
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
+
+/* Scroll for item menu - end */
